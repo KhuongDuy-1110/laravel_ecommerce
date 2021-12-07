01192a55a8832d9dd1b1d9ed42c9fb11bd992aa5
khuongpham-DD3 <khuong.pham@tda.company>
2021-12-07 17:48:25 +0700

    update0712

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,11 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Product;
+use Illuminate\Support\Facades\Redis;
+use Cache;
 
 class ProductController extends Controller
 {
     public function index()
     {
-        echo 'hihi';
+        // $data = Cache::remember('index.product',120, function(){
+        //     return Product::orderByDesc('id')->paginate(5);
+        // });
+        // $data = Cache::get('index.product');
+
+        $data = Product::orderByDesc('id')->paginate(5);
+        Redis::set('index.product',json_encode($data));
+        $get = Redis::get('index.product');
+
+        dd(json_decode($get));
+        
     }
 }
