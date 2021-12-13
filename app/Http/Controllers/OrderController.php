f28a10ac45821efdfe658907e1b575b19e2a8d23
khuongpham-DD3 <khuong.pham@tda.company>
2021-12-13 17:41:17 +0700

    update1312

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Events\NewOrder;
 
 class OrderController extends Controller
 {
     public function mailling(Request $request)
     {
-        MailController::confirmOrderMail($request->email);
-        
+        MailController::confirmOrderMail($request->email); 
+        event(new NewOrder($request->email));
+        return back();
     }
 }
