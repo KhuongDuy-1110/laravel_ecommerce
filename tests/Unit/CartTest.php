a5e133be99148ef1739df6703bb585359d40d9f1
khuongpham-DD3 <khuong.pham@tda.company>
2021-12-27 21:21:03 +0700

    home

--- tests/Unit/CartTest.php
+++ tests/Unit/CartTest.php
@@ -4,19 +4,19 @@ namespace Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
 use Mockery;
-// use Tests\TestCase;
 use App\Http\Controllers\CartController;
 
 class CartTest extends TestCase
 {
-    public function testAddCart()
+    public function testUpdateCart()
     {
         
         $cartController = Mockery::mock(CartController::class)->makePartial();
         $cartController->shouldReceive('AddCart')
-                        ->with()
-                        ->andReturn();
-
+                        ->with(5)
+                        ->andReturn(6);
+        $result = $cartController->updateCart(5);
+        $this->assertEquals(6,$result);
                         
     }
 }
