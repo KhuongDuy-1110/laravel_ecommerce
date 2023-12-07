43d47f4b2376adf1d555dedcaf5cc52bcc544970
khuongpd <khuong.pd@zinza.com.vn>
2023-12-07 11:14:09 +0700

    add database seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,14 +1,26 @@
 <?php
 
+use App\Models\Admin;
+use App\Models\Role;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    
+
     public function run()
     {
-        // $this->call(UserSeeder::class);
-        // $this->call(AdminSeeder::class);
-        $this->call(ProductSeeder::class);
+        $this->call(RoleSeeder::class);
+        $managerRole = Role::where('name','manager')->first();
+        $adminAccountData = [
+            'name' => 'Phạm Duy Khương',
+            'email' => 'khuong.pd@zinza.com.vn',
+            'password' => Hash::make('khuong@123'),
+        ];
+        $admin = Admin::updateOrCreate([
+            'email' => $adminAccountData['email']
+        ], $adminAccountData);
+        $admin->roles()->detach();
+        $admin->roles()->attach($managerRole->id);
     }
 }
---
+++ database/seeds/RoleSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Role;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data = [
+            [
+                'name' => 'manager'
+            ],
+            [
+                'name' => 'staff'
+            ],
+            [
+                'name' => 'user'
+            ]
+        ];
+        $roleData = Role::all();
+        if (empty($roleData)) {
+            Role::insert($data);
+        }
+    }
+}
